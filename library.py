c98ef33b155d70310a7966cfa45cdbe3accc7e58
Izazfalih <izazfalih@gmail.com>
2026-03-12 15:14:41 +0700

    Tugas Praktikum KB Pertemuan1

---
+++ library.py
@@ -0,0 +1,13 @@
+import math
+import datetime
+
+# Menggunakan library math
+angka = int(input("Masukkan angka: "))
+pangkat = math.factorial(angka, 2)
+
+print("Hasil pangkat 2 dari", angka, "adalah", pangkat)
+
+# Menggunakan library datetime
+sekarang = datetime.datetime.now()
+
+print("Tanggal dan waktu sekarang:", sekarang)
---
+++ struktur_data.py
@@ -0,0 +1,24 @@
+#List
+hindia = ["Cincin", "Ramai Sepi Bersama", "Membasuh"]
+
+print("Daftar lagu:", hindia)
+
+hindia.append("Untuk Apa")
+
+print("Setelah ditambah:", hindia)
+
+#Set
+angka = {1, 2, 3, 4}
+
+print("Data set:", angka)
+
+angka.remove(3)
+
+print("Setelah dihapus:", angka)
+
+#Tuple
+warna = ("merah", "biru", "hijau")
+
+print("Isi tuple:", warna)
+
+print("Warna pertama:", warna[0])
